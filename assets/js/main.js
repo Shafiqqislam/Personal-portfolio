db18d6acab4b37ca3533a9d5c16fc699726acf6d
Shafiqqislam <shafiqislam7901@gmail.com>
2021-05-03 17:13:59 +0600

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,19 @@ function sendMail(params){
         console.log("success",res.status)
     })
 }
-
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
 
 
 /*===== MENU SHOW =====*/ 
